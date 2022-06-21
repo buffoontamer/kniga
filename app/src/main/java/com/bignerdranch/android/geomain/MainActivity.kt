b9227b774a5c2e921813d22484bfa73c36e79483
buffoontamer <anna.machckarina@gmail.com>
2022-06-21 17:36:53 +0500

    1i2glavi

---
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.bignerdranch.android.geomain
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var trueButton : Button
+    private lateinit var falseButton : Button
+    private lateinit var nextButton : ImageButton
+    private lateinit var prevButton : ImageButton
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, true),
+        Question(R.string.question_africa, true),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+    private var currentIndex = 0
+
+    private fun  checkAnswer (userAnswer: Boolean){
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer){
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
+        questionTextView = findViewById(R.id.question_text_view)
+        trueButton.setOnClickListener{
+            checkAnswer(true)
+        }
+        falseButton.setOnClickListener{
+            checkAnswer(false)
+        }
+        nextButton.setOnClickListener{
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        prevButton.setOnClickListener{
+            if (currentIndex==0){
+                currentIndex=questionBank.size -1
+            }
+            currentIndex = (currentIndex - 1) % questionBank.size
+            updateQuestion()
+        }
+
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+    private fun updateQuestion(){
+        val questionTextResId = questionBank [currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+}
